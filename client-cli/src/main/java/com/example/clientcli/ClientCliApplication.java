47d66b52f86854b100f8fca72c8206c7a0a1f412
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-09 20:30:40 +0200

    client cli

---
+++ client-cli/src/main/java/com/example/clientcli/ClientCliApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientCliApplication.class, args);
+    }
+
+}
---
+++ client-cli/src/test/java/com/example/clientcli/ClientCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
