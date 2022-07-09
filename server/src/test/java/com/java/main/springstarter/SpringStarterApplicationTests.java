5c31a346a76b4187ec3904ff58bd2196d0ab05d6
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-07-09 21:28:15 +0200

    initiating server with auth + swagger + file manipulations

--- server/src/test/java/com/java/main/springstarter/SpringStarterApplicationTests.java
+++ server/src/test/java/com/java/main/springstarter/SpringStarterApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.server;
+package com.java.main.springstarter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class SpringStarterApplicationTests {
 
     @Test
     void contextLoads() {
